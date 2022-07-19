11659b1a9c7059621332455da19e2420893b6b1f
CKMolinga <ck.molinga@gmail.com>
2022-07-19 14:17:46 +0200

    Created Transactions controller

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post,} from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 
@@ -6,5 +6,14 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
+  @Post('initiate')
+  initiatePayment(@Body() body: CreateTransactionDto) {
+    // return this.transactionService.initiatePayment(body);
+  }
+
+  @Get('getpsp/:transaction_id')
+  getPsp(@Param('transaction_id') transaction_id: string) {
+    // return this.transactionService.getPsp(transaction_id);
+  }
  
 }

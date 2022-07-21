f37429d0a839afa5734ce22c015e4b73162dcfb9
CKMolinga <ck.molinga@gmail.com>
2022-07-21 10:48:56 +0200

    created payments DTO

--- src/payment/dto/create-payment.dto.ts
+++ src/payment/dto/create-payment.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreatePaymentDto {
+    @IsString()
+    @IsNotEmpty()
+    gateway: string;
+
+    @IsNotEmpty()
+    phone_number: string | number
+
+    @IsString()
+    @IsOptional()
+    notify_url: string | null;
+
+    @IsString()
+    @IsOptional()
+    name: string | null;
+}
